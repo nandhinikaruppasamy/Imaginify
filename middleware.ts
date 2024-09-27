33306637b1b1cadd06115a352064afeb30132128
nandhini-mygate <nandhini.k@mygate.in>
2024-09-27 16:03:22 +0530

    Clerk Webhooks

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    // Public route for webhook
+    '/api/webhooks/clerk', // Ensure this route does not require authentication
   ],
 };
